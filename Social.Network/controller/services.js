31869deea63bdfd7faa5ee21de79303ef80aad94
Petar Petrov <petarpetrovt@gmail.com>
2015-05-13 17:47:33 +0300

    * Added VS .sln;

---
+++ Social.Network/controller/services.js
@@ -0,0 +1,21 @@
+(function () {
+	"use strict";
+
+	var App = angular.module("App.services", []);
+
+	App.value('version', '0.1');
+	App.service("UtilSrvc", function () {
+		return {
+			isAString: function (o) {
+				return typeof o == "string" || (typeof o == "object" && o.constructor === String);
+			},
+			helloWorld: function (name) {
+				var result = "Hum, Hello you, but your name is too weird...";
+				if (this.isAString(name)) {
+					result = "Hello, " + name;
+				}
+				return result;
+			}
+		}
+	});
+}());
